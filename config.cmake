437a50de567cb87ed3bc7ef2557bb6f9ecad2caf
Falko Zurell <falko.zurell@here.com>
2015-10-09 12:17:47 +0200

    Changed Light Sensor Code to ubirch
    Using Leos code to talk to the ISL29125 as the Adafruit seemed to have issues with I2C.

--- config.cmake
+++ config.cmake
@@ -4,9 +4,9 @@ set(F_CPU 16000000)
 set(BAUD 115200)
 
 # set the programmer and the device to use (default is to use simavr)
-set(PROGRAMMER usbasp)
-set(SERIAL_DEV /dev/cu.SLAB_USBtoUART)
-#set(PROGRAMMER arduino)
+#set(PROGRAMMER usbasp)
+set(SERIAL_DEV /dev/cu.usbserial-DA00UCWL)
+set(PROGRAMMER arduino)
 #set(SERIAL_DEV /tmp/simavr-uart0)
 
 # only needed if we do floating point math and want to print floats with printf
@@ -15,4 +15,4 @@ set(SERIAL_DEV /dev/cu.SLAB_USBtoUART)
 set(EXTRA_LIBS "-Wl,--trace")
 
 # if you want to compile Arduino sketches, set this to the Arduino SDK dir (1.6.5)
-set(ARDUINO_SDK_PATH "/Applications/Arduino.app/Contents/Java" CACHE STRING "Arduino SDK Path")
+set(ARDUINO_SDK_PATH "/Users/fzurell/Applications/Arduino.app/Contents/Java" CACHE STRING "Arduino SDK Path")
